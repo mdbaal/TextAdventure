b85fa52cf248fc06508419be4feb9d35eb6626d1
Mrapplepie <mircobaalmans@live.nl>
2017-05-12 09:39:01 +0200

    direction add constructor

---
+++ library/actions/Direction.java
@@ -0,0 +1,36 @@
+package library.actions;
+
+/**
+ * Created by mirco on 5/10/2017.
+ */
+public enum Direction {
+
+    NORTH("To the north", 0 , 1 ) ,
+
+    EAST("To the east", 1 , 0 ) ,
+
+    SOUTH("To the south" , 0 , -1 ) ,
+
+    WEST("To the west" , -1 ,0 );
+
+    Direction(String Msg,int dX,int dY){
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- library/rooms/Room.java
+++ library/rooms/Room.java
@@ -4,6 +4,7 @@ package library.rooms;
  * Created by mirco on 5/10/2017.
  */
 public class Room {
+
     int x;
     int y;
 
